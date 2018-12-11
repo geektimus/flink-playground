489b77c60db3cb6d66ce0243b4b964e721f5e332
Geektimus <alexander.kno+github@gmail.com>
2018-12-11 13:59:39 +0100

    Add additional tests and small refactor

--- src/main/scala/com/codingmaniacs/flink/playground/batch/TextAnalyzer.scala
+++ src/main/scala/com/codingmaniacs/flink/playground/batch/TextAnalyzer.scala
@@ -1,23 +1,44 @@
 package com.codingmaniacs.flink.playground.batch
 
 import org.apache.flink.api.common.operators.Order
-import org.apache.flink.api.scala.{ExecutionEnvironment, _}
+import org.apache.flink.api.scala._
 
 object TextAnalyzer {
 
-  val environment: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+  val MIN_WORD_LENGTH = 3
 
-  def countWords(fileName: String): Seq[(String, Int)] = {
+  def countWords(fileName: String)(implicit environment: ExecutionEnvironment): Seq[(String, Int)] = {
     val dataSet = environment.readTextFile(fileName)
-    val count = dataSet
-      .flatMap(line => line.toLowerCase.split("\\W+"))
-      .filter(word => word.nonEmpty && word.length > 3)
-      .map(word => (word, 1))
-      .groupBy(0)
-      .sum(1)
-      .sortPartition(1, Order.DESCENDING).setParallelism(1)
-
-    count.collect()
+    val words = transformDataSet(dataSet)
+    words.collect()
   }
+
+  def countTopNWords(fileName: String, numberOfWords: Int)(implicit environment: ExecutionEnvironment): Seq[(String, Int)] = {
+
+    val dataSet = environment.readTextFile(fileName)
+
+    // First will return a the first n elements from this data set, but It doesn't guarantee the order if the
+    // parallelism is greater than 1
+    val words = transformDataSet(dataSet).first(numberOfWords)
+
+    words.collect().sortBy(_._1)
+  }
+
+  val isValidWord: String => Boolean = {
+    case w if w == null || w.trim.isEmpty => false
+    case w if w.length <= MIN_WORD_LENGTH => false
+    case _ => true
+  }
+
+  val transformDataSet: DataSet[String] => DataSet[(String, Int)] = (baseData: DataSet[String]) => {
+    baseData
+      .flatMap(line => line.toLowerCase.split("\\W+"))
+      .filter(word => isValidWord(word))
+      .map(word => (word, 1))
+      .groupBy("_1")
+      .sum("_2").setParallelism(1)
+      .sortPartition("_2", Order.DESCENDING)
+  }
+
 
 }
--- src/test/scala/com/codingmaniacs/flink/playground/batch/TextAnalyzerSpec.scala
+++ src/test/scala/com/codingmaniacs/flink/playground/batch/TextAnalyzerSpec.scala
@@ -1,22 +1,69 @@
 package com.codingmaniacs.flink.playground.batch
 
+import org.apache.flink.api.scala.ExecutionEnvironment
 import org.specs2.mutable.Specification
 
 class TextAnalyzerSpec extends Specification {
 
+  "The simple word validator" should {
+    "handle null a not valid word" in {
+      val word: String = null
+      val result = TextAnalyzer.isValidWord(word)
+      result must beFalse
+    }
+
+    "handle empty string a not valid word" in {
+      val word: String = ""
+      val result = TextAnalyzer.isValidWord(word)
+      result must beFalse
+    }
+
+    "handle an string containing spaces a not valid word" in {
+      val word: String = "           "
+      val result = TextAnalyzer.isValidWord(word)
+      result must beFalse
+    }
+
+    "handle an string with a size less than the threshold as not valid word" in {
+      val word: String = "sun"
+      val result = TextAnalyzer.isValidWord(word)
+      result must beFalse
+    }
+
+    "handle an string with a size greater than the threshold as valid word" in {
+      val word: String = "awesome"
+      val result = TextAnalyzer.isValidWord(word)
+      result must beTrue
+    }
+  }
+
   "The basic TextAnalyzer" should {
     "count the number of words in a empty file" in {
       val emptyFile = "src/test/resources/empty-file.txt"
-      val count = TextAnalyzer.countWords(emptyFile)
-      count must have size 0
-      count should containTheSameElementsAs(List())
+      implicit val environment: ExecutionEnvironment = ExecutionEnvironment.createLocalEnvironment()
+
+      val words = TextAnalyzer.countWords(emptyFile)
+      words must have size 0
+      words should containTheSameElementsAs(List())
     }
 
     "count the number of words in a file" in {
-      val emptyFile = "src/test/resources/lorem-ipsum.txt"
-      val count = TextAnalyzer.countWords(emptyFile)
-      count must have size 81
-      count must contain (atLeast(("lorem",6), ("from",6), ("ipsum",6), ("latin",3), ("classical",2), ("comes",2)))
+      val ipsumFile = "src/test/resources/lorem-ipsum.txt"
+      implicit val environment: ExecutionEnvironment = ExecutionEnvironment.createLocalEnvironment()
+
+      val words = TextAnalyzer.countWords(ipsumFile)
+      words must have size 81
+      words must contain(atLeast(("lorem", 6), ("from", 6), ("ipsum", 6), ("latin", 3), ("classical", 2), ("comes", 2)))
+    }
+
+    "return the top N words in a file" in {
+      val ipsumFile = "src/test/resources/lorem-ipsum.txt"
+      implicit val environment: ExecutionEnvironment = ExecutionEnvironment.createLocalEnvironment()
+
+      val numberOfWords = 6
+      val words = TextAnalyzer.countTopNWords(ipsumFile, numberOfWords)
+      words must have size numberOfWords
+      words must contain(atMost(("from", 6), ("ipsum", 6), ("lorem", 6), ("latin", 3), ("cicero", 2), ("line", 2)))
     }
   }
 }
